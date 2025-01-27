05dbc5375ff8524210ba5df1de34c74809723a50
guillex387 <guillesiglez387@gmail.com>
2025-01-27 18:59:23 +0000

    Added activities

--- src/views/EventCalendar.tsx
+++ src/views/EventCalendar.tsx
@@ -1,12 +1,14 @@
+import Activities from "@/components/Activities";
 import Calendar from "@/components/Calendar.tsx";
 import type { FC } from "react";
 
 const EventCalendarView: FC = () => (
   <article
     id="calendar"
-    className="flex justify-center items-center mb-[80px] w-full px-[20px] pt-[100px]"
+    className="flex flex-col gap-4 justify-center items-center mb-[80px] w-full px-[20px] pt-[100px]"
   >
     <Calendar />
+    <Activities />
   </article>
 );
 
